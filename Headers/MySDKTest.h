76cd8ed6f3512ba4bc2e83c4fbebeb42a98d489a
renpeng <huyang_work@163.com>
2020-10-07 11:00:41 +0800

    first commit

---
+++ Headers/MySDKTest.h
@@ -0,0 +1,17 @@
+//
+//  MySDKTest.h
+//  TestSDK
+//
+//  Created by huyang on 2020/9/18.
+//  Copyright © 2020 test. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MySDKTest : NSObject
++ (void)testLog;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Headers/TestViewController.h
@@ -0,0 +1,17 @@
+//
+//  TestViewController.h
+//  TestSDK
+//
+//  Created by huyang on 2020/9/21.
+//  Copyright © 2020 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
